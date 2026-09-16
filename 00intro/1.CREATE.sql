b40e470360c841f97ec5d3930bb639a7e68d1191
Reza Behzadi <rbehzadi@localhost.localdomain>
2026-09-16 21:33:57 +0200

    Skapa tabeller

--- 00intro/1.CREATE.sql
+++ 00intro/1.CREATE.sql
@@ -1,4 +1,8 @@
-# Mappar
+# CREATE table "customers":
 
-Vi ska skapa undermappar:
- - touch 1.CREATE.sql
+CREATE TABLE customers (
+customer_id SERIAL PRIMARY KEY,
+customer_name VARCHAR (100) NOT NULL,
+city VARCHAR (50),
+signup_date DATE
+);
