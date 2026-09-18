c1bc40591fe6b779b6b50ec02f3bcc4a3c1c5b93
Reza Behzadi <rbehzadi@localhost.localdomain>
2026-09-18 15:06:10 +0200

    Ny mapp

--- 00intro/1.CREATE.sql
+++
@@ -1,8 +0,0 @@
-# CREATE table "customers":
-
-CREATE TABLE customers (
-customer_id SERIAL PRIMARY KEY,
-customer_name VARCHAR (100) NOT NULL,
-city VARCHAR (50),
-signup_date DATE
-);
--- 00intro/2.INSERT.sql
+++
@@ -1,4 +0,0 @@
-# Mata information i tabellen "customer":
-
-INSERT INTO customers (customer_name, city, signup_date) VALUES
-('Alice Johnson', 'New York', '2024-01-15')
